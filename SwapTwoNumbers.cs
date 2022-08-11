cf38b29efd2a870e140807676794ba7f8e500c22
Wilbert Tosoy <brownabelard@gmail.com>
2022-08-11 20:59:11 +0800

    Swap two numbers C# file.

---
+++ SwapTwoNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SwapTwoNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            Console.Write("Enter first number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Before Swapping: a = {a}, b = {b}");
+
+            c = a;
+            a = b;
+            b = c;
+
+            Console.WriteLine($"After Swapping: a = {a}, b = {b}");
+            Console.ReadKey();
+        }
+    }
+}
