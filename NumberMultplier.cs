facb96f248cbbeab12a7deb03580dd5a2e80362c
Wilbert Tosoy <brownabelard@gmail.com>
2022-08-11 22:06:47 +0800

    takes a number as input and prints its multiplication table.

---
+++ NumberMultplier.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NumberMultplier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            Console.Write("Enter a number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"{number} * {i} = {number * i}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
