0485eba06d430bd0d2593701f30bdf5e1fd4e9a0
Wilbert Tosoy <brownabelard@gmail.com>
2022-08-11 21:16:22 +0800

    Adding, subtracting, multiplying and dividing two numbers C# file.

---
+++ AddingSubtractingMultiplyingDividingOfTwoNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AddingSubtractingMultiplyingDividingOfTwoNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+
+            Console.Write("Enter the first number: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"{a} + {b} = {a + b:F2}\n" +
+                $"{a} - {b} = {a - b:F2}\n" +
+                $"{a} * {b} = {a * b:F2}\n" +
+                $"{a} % {b} = {a / b:F2}");
+
+            Console.ReadKey();
+        }
+    }
+}
