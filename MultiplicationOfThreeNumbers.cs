abeaf5dcdd23945d5a12f1f3ac7466176f30aae6
Wilbert Tosoy <brownabelard@gmail.com>
2022-08-11 21:06:14 +0800

    Multiplication of three numbers C# file.

---
+++ MultiplicationOfThreeNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MultiplicationOfThreeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+
+            Console.Write("Enter the first number: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the third number: ");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"{a} x {b} x {c} = {a * b * c:F2}");
+            Console.ReadKey();
+        }
+    }
+}
