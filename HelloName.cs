56519d1742c49cacc5513b6577f8a7fe3647b4d9
Wilbert Tosoy <97012041+Wilbert-Tosoy@users.noreply.github.com>
2022-08-11 20:31:21 +0800

    Add files via upload

---
+++ HelloName.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Hello___Name__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Name: ");
+            string name = Convert.ToString(Console.ReadLine());
+            Console.WriteLine($"Hello {name}!");
+
+            Console.ReadKey();
+        }
+    }
+}
