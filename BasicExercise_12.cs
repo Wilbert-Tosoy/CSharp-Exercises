9a63483f0c8bb0737a99fb11b3bd17e24a2bdaaa
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-12 09:33:29 +0800

    Write a C# program to that takes a number as input and display it four times in a row (separated by blank spaces), and then four times in the next row, with no separation. It should be done two times.

---
+++ BasicExercise_12.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BasicExercise_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            Console.Write("Enter a digit: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        Console.Write(number);
+                    }
+                    Console.WriteLine();
+                } 
+                else
+                {
+                    for (int k = 0; k < 4; k++)
+                    {
+                        Console.Write($"{number} ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
