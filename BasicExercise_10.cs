19791133ef15be352bc5d29b238f79149d19f49b
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-12 09:18:26 +0800

    Write a C# Sharp program to that takes three numbers(a,b,c) as input and print the output of (a+b)c and a.b + b.c.

---
+++ BasicExercise_10.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BasicExercise_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+
+            Console.Write("Enter first number: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter third number: ");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Result of specified numbers {a}, {b} and {c}," +
+                $"(a + b)c is {((a + b) * c):F2}" +
+                $" and ((a * b) + (b * c)) is {((a * b) + (b * c)):F2}");
+            Console.ReadKey();
+        }
+    }
+}
