381283df2f93f0cf08a7337d31d93da83d7552e2
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-12 11:44:26 +0800

    Write a C# program remove specified a character from a non-empty string using index of a character.

---
+++ BasicExercise_15.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BasicExercise_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word;
+            int index;
+
+            Console.Write("Enter a word: ");
+            word = Convert.ToString(Console.ReadLine());
+            Console.WriteLine(word);
+
+            Console.Write("Letter-index to be remove: ");
+            index = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{word.Remove(index, index)}");
+            Console.ReadKey();
+        }
+    }
+}
