9b37c36ad3ada6ce9d519445defcfb04eb2194c0
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-12 18:30:29 +0800

    Write a C# program to create a new string from a given string (length 1 or more ) with the first character added at the front and back.

---
+++ BasicExercise_17.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+namespace BasicExercise_17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word;
+
+            Write("Enter a string: ");
+            word = Convert.ToString(ReadLine());
+
+            string firstLetter = word.Substring(0, 1);
+            WriteLine($"{firstLetter}{word}{firstLetter}");
+            ReadKey();
+        }
+    }
+}
