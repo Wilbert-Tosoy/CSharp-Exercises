15540d7ecc1b4d0e7716322cf82465a2c0ff5809
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-12 10:03:27 +0800

    Write a C# program that takes a number as input and then displays a rectangle of 10 columns wide and 10 rows tall using that digit.

---
+++ BasicExercise_13.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace BasicExercise_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            Console.Write("Enter a digit: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (i > 0 && i < 9)
+                    {
+                        if (j != 0 && j != 9)
+                        {
+                            Console.Write(" ");
+                        }
+                        else
+                        {
+                            Console.Write(number);
+                        }
+                    } 
+                    else
+                    {
+                        Console.Write(number);
+                    }
+                }
+                Console.WriteLine();    
+            }
+            Console.ReadKey();
+        }
+    }
+}
