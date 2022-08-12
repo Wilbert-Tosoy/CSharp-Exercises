a130201134b664c76925bbe24afd548d80beffe7
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-12 16:38:47 +0800

    Write a C# program to create a new string from a given string where the first and last characters will change their positions.

---
+++ BasicExercise_16.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BasicExercise_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+
+            Console.Write("Enter a string: ");
+            str = Convert.ToString(Console.ReadLine());
+
+            if (str.Length > 1)
+            {
+                Console.WriteLine(str);
+                Console.WriteLine($"{str.Substring(str.Length - 1)}{str.Substring(1, str.Length - 2)}{str.Substring(0, 1)}");
+            }
+            else
+            {
+                Console.WriteLine(str); 
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
