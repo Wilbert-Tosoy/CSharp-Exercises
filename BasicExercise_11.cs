4f94d105eeeeac70b5d11b616ec7560daa83f486
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-12 09:22:07 +0800

    Write a C# Sharp program that takes an age (for example 20) as input and prints something as "You look older than 20".

---
+++ BasicExercise_11.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BasicExercise_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+
+            Console.Write("Enter your age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"You look older than {age}");
+            Console.ReadKey();
+        }
+    }
+}
