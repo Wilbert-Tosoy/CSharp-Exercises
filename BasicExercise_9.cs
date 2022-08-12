2b6466135e5563c5b8863d5fdf61c4b1b88b2a4c
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-12 09:09:36 +0800

    Write a C# Sharp program that takes four numbers as input to calculate and print the average.

---
+++ BasicExercise_9.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Runtime.CompilerServices;
+
+namespace BasicExercise_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, d;
+
+            Console.Write("Enter the First number: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the Second number: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the Third number: ");
+            c = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the Fourth number: ");
+            d = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"The average of {a}, {b}, {c}, {d} is: {(a + b + c + d) / 4:F2}");
+            Console.ReadKey();
+        }
+    }
+}
