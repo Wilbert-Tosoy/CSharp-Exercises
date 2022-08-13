cc15d8cc9a5775bc761cefc790c9f7bae40060dd
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-14 07:58:13 +0800

    Find the longest word in a string.

---
+++ BasicExercise_24.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static System.Console;
+
+namespace BasicExercise_24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+
+            Write("Enter a sentence: ");
+            str = ReadLine();
+
+            string[] words = str.Split(new[] { " " }, StringSplitOptions.None);
+            string word = "";
+            int ctr = 0;
+            foreach (String s in words)
+            {
+                if (s.Length > ctr)
+                {
+                    word = s;
+                    ctr = s.Length;
+                }
+            }
+
+            WriteLine(word);
+
+            ReadKey();
+        }
+    }
+}
