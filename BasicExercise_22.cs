612eb4ed51b62016ca164fedfa9608eb36a01291
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-14 07:45:40 +0800

    Check if an given integer is within 20 of 100 or 200.

---
+++ BasicExercise_22.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+namespace BasicExercise_22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+
+            Write("Enter a number: ");
+            x = ToInt32(ReadLine());
+
+            if (Math.Abs(x - 100) <= 20 || Math.Abs(x - 200) <= 20)
+            {
+                WriteLine(true);
+            }
+            else
+            {
+                WriteLine(false);
+            }
+
+            ReadKey();
+        }
+    }
+}
