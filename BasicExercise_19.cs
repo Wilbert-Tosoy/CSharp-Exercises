b458faef4f103db783a73785bbb7032808a200a5
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-13 20:44:37 +0800

    Write a C# program to compute the sum of two given integers, if two values are equal then return the triple of their sum.

---
+++ BasicExercise_19.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+
+namespace BasicExercise_19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+
+            Write("Enter the first number: ");
+            x = Convert.ToInt32(ReadLine());
+            Write("Enter the second number: ");
+            y = Convert.ToInt32(ReadLine());
+
+            if (x == y)
+            {
+                WriteLine(((x + y) * 3));
+            }
+            else
+            {
+                WriteLine(x + y);
+            }
+
+            ReadKey();
+        }
+    }
+}
