cb7bee4846a5d8c78460e7043239420a30314ba6
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-13 21:13:49 +0800

    Write a C# program to check the sum of the two given integers and return true if one of the integer is 20 or if their sum is 20.

---
+++ BasicExercise_21.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+namespace BasicExercise_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+
+            do
+            {
+                Write("Enter the first number: ");
+                x = ToInt32(ReadLine());
+                if (x == 20)
+                {
+                    WriteLine(x == 20);
+                    break;
+                }
+
+                Write("Enter the second number: ");
+                y = ToInt32(ReadLine());
+                if (y == 20)
+                {
+                    WriteLine(y == 20);
+                    break;
+                }
+
+                if (x + y == 20)
+                {
+                    WriteLine(x + y == 20);
+                    break;
+                }
+                else
+                {
+                    WriteLine(x + y == 20);
+                    break;
+                }
+ 
+            } while (true);
+
+            ReadKey();
+        }
+    }
+}
