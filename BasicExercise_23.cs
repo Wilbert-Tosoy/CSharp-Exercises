8c0b892ba9ff0940b8e6047228374e44d6e63818
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-14 07:53:04 +0800

    Convert a string into lowercase.

---
+++ BasicExercise_23.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+namespace BasicExercise_23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+
+            Write("Enter a sentence: ");
+            str = ReadLine();
+
+            WriteLine(str.ToLower());
+
+            ReadKey();
+        }
+    }
+}
