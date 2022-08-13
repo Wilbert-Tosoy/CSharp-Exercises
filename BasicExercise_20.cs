0705e372588cf9f606784732210569623419f6e0
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-13 20:57:07 +0800

    Write a C# program to get the absolute value of the difference between two given numbers. Return double the absolute value of the difference if the first number is greater than second number.

---
+++ BasicExercise_20.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+namespace BasicExercise_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+
+            Write("Enter the first number: ");
+            x = ToInt32(ReadLine());
+            Write("Enter the second number: ");
+            y = ToInt32(ReadLine());
+
+            if (x > y)
+            {
+                Absolute(x, y);
+            } 
+            else
+            {
+                WriteLine(Math.Abs(x - y));
+            }
+
+            ReadKey();
+        }
+
+        public static void Absolute(int x, int y)
+        {
+            WriteLine(Math.Abs((x - y) * 2));
+            
+        }
+    }
+}
