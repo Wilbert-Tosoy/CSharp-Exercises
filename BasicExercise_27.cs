36a85546223f9e074723f76c286763c8d220511a
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-14 08:50:04 +0800

    Compute the sum of the digits of an integer.

---
+++ BasicExercise_27.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+namespace BasicExercise_27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, sum = 0;
+
+            Write("Enter a number: ");
+            x = ToInt32(ReadLine());
+
+            while (x != 0)
+            {
+                sum += (x % 10);
+                x /= 10;
+            }
+
+            WriteLine($"Sum of the digits: {sum}");
+
+            ReadKey();
+        }
+    }
+}
