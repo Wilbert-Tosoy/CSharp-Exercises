f04e69f0e2c4ce22d4825acedb57cdf3411f5b43
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-14 08:39:14 +0800

    Compute the sum of the first 500 prime numbers.

---
+++ BasicExercise_26.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using static System.Console;
+
+namespace BasicExercise_26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long sum = 0;
+            int counter = 0;
+            int number = 2;
+
+            while (counter < 500)
+            {
+                if (PrimeChecker(number))
+                {
+                    sum += number;
+                    counter++;
+                }
+                number++;
+            }
+
+            WriteLine($"Sum of the first 500 prime numbers: {sum}");
+
+            ReadKey();
+        }
+
+        public static bool PrimeChecker(int number)
+        {
+            int x = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 2; i <= x; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
