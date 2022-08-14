0a162c55a087fe97147f4ed9ab526465687b53a4
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-14 08:02:34 +0800

    Print the odd numbers from 1 to 99.

---
+++ BasicExercise_25.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+namespace BasicExercise_25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    WriteLine(i);
+                }               
+            }
+
+            ReadKey();
+        }
+    }
+}
