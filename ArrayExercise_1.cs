92787dc4b7d5de11f15675440be3a2fc307f4716
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-15 21:25:03 +0800

    Store elements in an array and print it.

---
+++ ArrayExercise_1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+namespace ArrayExercise_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+
+            // Read the inputs.
+            WriteLine("Enter 10 elements in the array.");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Write($"Element - [0] : ");
+                array[i] = ToInt32(ReadLine());
+            }
+
+            // Print the inputs.
+            for (int i = 0; i < array.Length; i++)
+            {
+                WriteLine($"[0] - {array[i]}");
+            }
+
+            ReadKey();
+        }
+    }
+}
