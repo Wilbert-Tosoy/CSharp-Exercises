9b02cd3b12e8f3e17982e8cdf5d1492a6a8c3130
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-15 21:44:26 +0800

    Read n number of values in an array and display it in reverse order.

---
+++ ArrayExercise_2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+namespace ArrayExercise_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Number of elements to store in the array: ");
+            int number = ToInt32(ReadLine());
+
+            int[] array = new int[number];
+
+            // Read the inputs.
+            WriteLine($"Input {number} number of elements in the array.");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Write("Element - [0] : ");
+                array[i] = ToInt32(ReadLine());
+            }
+
+            // Prints the values in right order.
+            WriteLine("The values store into the array are: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Write($"{array[i]} ");
+            }
+
+            // Print the inputs in reverse.
+            WriteLine($"\nThe values store into the array in reverse are: ");
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                Write($"{array[i]} ");
+            }
+
+            ReadKey();
+        }
+    }
+}
