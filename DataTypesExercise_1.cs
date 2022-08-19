af1e580b80a95a17139778d859ae5f92898dc740
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-20 07:11:49 +0800

    Takes three letters as input and display them in reverse order.

---
+++ DataTypesExercise_1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+namespace DataTypesExercise_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] letters = new char[3];
+
+            for (int i = 0; i < letters.Length; i++)
+            {
+                Write("Enter letter: ");
+                letters[i] = ToChar(ReadLine());
+            }
+
+            Array.Reverse(letters);
+            for (int i = 0; i < letters.Length; i++)
+            {
+                Write($"{letters[i]} ");
+            }
+
+            ReadKey();
+        }
+    }
+}
