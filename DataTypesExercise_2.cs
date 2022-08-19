6f623aebafae392b9965215e56dbdf88e6a4f06f
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-20 07:19:41 +0800

    Takes a number and a width also a number, as input and then displays a triangle of that width, using that number.

---
+++ DataTypesExercise_2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+namespace DataTypesExercise_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width, height, number;
+
+            Write("Enter a number: ");
+            number = ToInt32(ReadLine());
+
+            Write("Enter the desired width: ");
+            width = ToInt32(ReadLine());
+
+            height = width;
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Write(number);
+                }
+                WriteLine("");
+                width--;
+            }
+
+            ReadKey();
+        }
+    }
+}
