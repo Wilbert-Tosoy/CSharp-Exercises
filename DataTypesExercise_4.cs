dea1eb3efa64de03a89b3057ecb67b3afbf95349
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-20 07:52:09 +0800

    Takes two numbers as input and perform an operation (+,-,*,x,/) on them and displays the result of that operation.

---
+++ DataTypesExercise_4.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+namespace DataTypesExercise_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            char operation;
+
+            Write("Enter the first number: ");
+            x = ToDouble(ReadLine());
+
+            Write("Enter operation: ");
+            operation = ToChar(ReadLine());
+
+            Write("Enter the second number: ");
+            y = ToDouble(ReadLine());
+
+            switch (operation)
+            {
+                case '+':
+                    WriteLine($"{x} + {y} = {x + y}");
+                    break;
+                case '-':
+                    WriteLine($"{x} - {y} = {x - y}");
+                    break;
+                case '*':
+                    WriteLine($"{x} * {y} = {x * y}");
+                    break;
+                case '/':
+                    WriteLine($"{x} / {y} = {x / y}");
+                    break;
+                default:
+                    WriteLine("Syntax Error!");
+                    break;
+            }
+
+            ReadKey();
+        }
+    }
+}
