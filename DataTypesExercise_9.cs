05294a06c24f63b3e3c80c22c58dab68b8a7b0fb
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-20 21:34:04 +0800

    Check an input if it is a vowel, a digit, or any other symbol

---
+++ DataTypesExercise_9.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+namespace DataTypesExercise_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char x;
+
+            Write("Enter a symbol: ");
+            x = ToChar(ReadLine());
+
+            if (Char.IsLower(x))
+            {
+                if ((x == 'a') || (x == 'e') || (x == 'i') || (x == 'o') || (x == 'u'))
+                {
+                    WriteLine("It is a lowercase vowel.");
+                } 
+                else
+                {
+                    WriteLine("Is is a lowercase consonant.");
+                }
+            }
+            else if (Char.IsUpper(x))
+            {
+                if ((x == 'A') || (x == 'E') || (x == 'I') || (x == 'O') || (x == 'U'))
+                {
+                    WriteLine("It is an uppercase vowel.");
+                }
+                else
+                {
+                    WriteLine("Is is an uppercase consonant.");
+                }
+            }
+            else if (Char.IsNumber(x))
+            {
+                WriteLine("It is a number.");
+            }
+            else
+            {
+                WriteLine("It is another symbol.");
+            }
+
+            ReadKey();
+        }
+    }
+}
