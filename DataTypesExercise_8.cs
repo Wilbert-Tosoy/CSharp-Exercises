f9ebe3297a6e6f9fc2dfff2f35033a2f3f69ea0e
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-20 20:57:59 +0800

    Calculate and display the area and volume of a sphere.

---
+++ DataTypesExercise_8.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+namespace DataTypesExercise_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius;
+
+            Write("Enter radius: ");
+            radius = ToDouble(ReadLine());
+
+            WriteLine($"Volume: {(4.00/3.00) * Math.PI * Math.Pow(radius, 3)}");
+            WriteLine($"Area: {4.00 * Math.PI * Math.Pow(radius, 2)}");
+
+            ReadKey();
+        }
+    }
+}
