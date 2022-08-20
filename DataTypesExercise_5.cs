03265ab723f165de69dabec5552e65b0c034914a
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-20 13:31:09 +0800

    Take the radius of a circle as input and calculate the perimeter and area of the circle.

---
+++ DataTypesExercise_5.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+namespace DataTypesExercise_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius;
+
+            Write("Enter radius: ");
+            radius = ToDouble(ReadLine());
+
+            WriteLine($"Circumference: {2 * Math.PI * radius:F2}");
+
+            ReadKey();
+        }
+    }
+}
