2ad499d5b84b9df86f925cc1ab0a5699ef74f2af
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-21 07:47:45 +0800

    Take two numbers as input and returns true or false when both numbers are even or odd.

---
+++ DataTypesExercise_10.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+namespace DataTypesExercises_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+
+            Write("Enter first number: ");
+            x = ToInt32(ReadLine());
+            Write("Enter second number: ");
+            y = ToInt32(ReadLine());
+
+            if (x % 2 == 0 && y % 2 == 0)
+            {
+                Write("True");
+            }
+            else
+            {
+                Write("False");
+            }
+
+            ReadKey();
+        }
+    }
+}
