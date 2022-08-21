3ea8157e19ca09b6cbdc1613afe02cbfc906a6a6
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-21 09:06:18 +0800

    Check whether a given number is positive or negative.

---
+++ ConditionalStatementExercise_3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+namespace ConditionalStatementExercise_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            Write("Enter a number: ");
+            number = ToInt32(ReadLine());
+
+            if (number > 0)
+            {
+                WriteLine($"{number} is a positive number.");
+            }
+            else if (number < 0)
+            {
+                WriteLine($"{number} is a negative number.");
+            }
+            else
+            {
+                WriteLine($"{number} is a zero.");
+            }
+
+            ReadKey();
+        }
+    }
+}
