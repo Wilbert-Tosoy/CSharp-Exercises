627470a1e943eb2029440839e956e2989063ba6d
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-21 09:15:40 +0800

    Find whether a given year is a leap year or not.

---
+++ ConditionalStatementExercise_4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+namespace ConditionalStatementExercise_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+
+            Write("Enter Year: ");
+            year = ToInt32(ReadLine());
+
+            string leap = year % 4 == 0 ? ($"{year} is a leap year") : ($"{year} is a not a leap year");
+            WriteLine(leap);
+
+            ReadKey();
+        }
+    }
+}
