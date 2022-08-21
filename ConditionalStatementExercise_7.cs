b810f7fc51c63c62295c1dd038a54dd36edd6225
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-22 07:57:19 +0800

    Accept the height of a person in centimeter and categorize them.

---
+++ ConditionalStatementExercise_7.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+namespace ConditionalStatementExercise_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double height;
+
+            Write("Enter height in cm: ");
+            height = ToDouble(ReadLine());
+
+            if (height >= 165.00 && height <= 195.00)
+            {
+                WriteLine("The person is taller.");
+            }
+            else if (height >= 150.00 && height <= 165.00)
+            {
+                WriteLine("The person is average height.");
+            }
+            else if (height < 150.00)
+            {
+                WriteLine("The person is dwarf.");
+            }
+            else
+            {
+                WriteLine("Abnormal height.");
+            }
+
+            ReadKey();
+        }
+    }
+}
