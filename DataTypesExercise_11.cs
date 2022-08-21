749d47e398b192fb5fd2d0dd56a26ac1cea35df4
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-21 08:18:30 +0800

    Take a decimal number as input and displays its equivalent in binary form.

---
+++ DataTypesExercise_11.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+namespace DataTypesExercise_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string result;
+            string answer;
+            int num;
+
+            Write("Enter a number: ");
+            answer = ReadLine();
+
+            num = ToInt32(answer);
+            result = "";
+
+            while (num > 1)
+            {
+                int remainder = num % 2;
+                result = Convert.ToString(remainder) + result;
+                WriteLine(result);
+                num /= 2;
+            }
+
+            result = Convert.ToString(num) + result;
+            WriteLine($"Binary: {result}");
+
+            ReadKey();
+        }
+    }
+}
