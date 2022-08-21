066544fab8142bc7da95be8b8990c4ffcbbc2673
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-22 07:49:35 +0800

    Display the value of an integer n is 1,0 and -1 for the value of an integer m.

---
+++ ConditionalStatementExercise_6.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+namespace ConditionalStatementExercise_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int m, n;
+
+            Write("Enter the value of m: ");
+            m = ToInt32(ReadLine());
+
+            if (m > 0)
+            {
+                n = 1;
+                WriteLine($"The value of n = {n}");
+            }
+            else if (m < 0)
+            {
+                n = -1;
+                WriteLine($"The value of n = {n}");
+            }
+            else
+            {
+                n = 0;
+                WriteLine($"The value of n = {n}");
+            }
+
+            ReadKey();
+        }
+    }
+}
