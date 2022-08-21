599abc9b81ce4ee6545fc5f3d98086b6e748ef39
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-21 09:01:07 +0800

    Check whether a given number is even or odd.

---
+++ ConditionalStatementExercise_2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+namespace ConditionalStatementExercise_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            Write("Enter a number: ");
+            number = ToInt32(ReadLine());
+
+            if (number % 2 == 0)
+            {
+                WriteLine($"{number} is an even number.");
+            }
+            else
+            {
+                WriteLine($"{number} is an odd number.");
+            }
+
+            ReadKey();
+        }
+    }
+}
