df2fb31e8193e158eda35eb2a04c7467ca1e269d
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-22 07:42:14 +0800

    Determine if a specific age is eligible for casting the vote.

---
+++ ConditionalStatementExercise_5.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+namespace ConditionalStatementExercise_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+
+            Write("Enter age: ");
+            age = ToInt32(ReadLine());
+
+            WriteLine(age >= 18 ? "Congratulations! You are elegible for casting your vote" : 
+                "Sorry, You are not elegible for casting your vote.");
+
+            ReadKey();
+        }
+    }
+}
