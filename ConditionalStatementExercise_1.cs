95acb9609d528cad95a90ea3da4f143e0002d975
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-21 08:51:07 +0800

    Take two integers and check whether they are equal or not.

---
+++ ConditionalStatementExercise_1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+namespace ConditionalStatementExercise_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+
+            Write("Enter first number: ");
+            x = ToInt32(ReadLine());
+            Write("Enter second number: ");
+            y = ToInt32(ReadLine());
+
+            if (x == y)
+            {
+                WriteLine($"{x} and {y} are equal.");
+            }
+            else
+            {
+                WriteLine($"{x} and {y} are not equal.");
+            }
+
+            ReadKey();
+        }
+    }
+}
