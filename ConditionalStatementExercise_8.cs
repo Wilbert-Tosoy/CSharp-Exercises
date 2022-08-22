5598a085fd71c2e032a4da3b678720f53c29c4cb
Wilbert-Tosoy <brownabelard@gmail.com>
2022-08-22 08:03:31 +0800

    Find the largest of three numbers.

---
+++ ConditionalStatementExercise_8.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+namespace ConditionalStatementExercise_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            Write("Enter the value of the first number: ");
+            a = ToInt32(ReadLine());
+            Write("Enter the value of the second number: ");
+            b = ToInt32(ReadLine());
+            Write("Enter the value of the third number: ");
+            c = ToInt32(ReadLine());
+
+            if (a > b && a > c)
+            {
+                WriteLine("The 1st number is the greatest among three.");
+            } 
+            else if (b > a && b > c)
+            {
+                WriteLine("The 2nd number is the greatest among three.");
+            }
+            else
+            {
+                WriteLine("The 3rd number is the greates among three.");
+            }
+
+            ReadKey();
+        }
+    }
+}
